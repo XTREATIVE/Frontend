3cbefbb5e8ac24559ca46710c6e38dbad74bde48
tracy <keeyatracy@gmail.com>
2025-02-20 09:24:47 +0300

    added some folders

--- web-app/Xtreative_market/App.js
+++ web-app/Xtreative_market/App.js
@@ -18,3 +18,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
